c516e931a222c04257bc3c778aacb20857e6f255
Aleksandr Karkhunen <ajlvatanabe@gmail.com>
2025-09-15 15:32:21 +0300

    Изменено значение деления без остатка на 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val likes = 14
-    val result = if (likes % 10 != 10) println("Понравилось ${likes} людям") else println("Понравилось ${likes} человеку")
+    val likes = 1
+    val result = if (likes % 10 != 1) println("Понравилось ${likes} людям") else println("Понравилось ${likes} человеку")
 
 }
