31f20a4de12705b09a7105e4c218efce29bfec7e
Aleksandr Karkhunen <ajlvatanabe@gmail.com>
2025-09-16 17:42:44 +0300

    Скорректировано условие при обработке числе заканчивающихся на 11

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val likes = 1
-    val result = if (likes % 10 != 1) println("Понравилось ${likes} людям") else println("Понравилось ${likes} человеку")
+    val likes = 211
+    val result = if (likes % 100 != 11 && likes % 10 == 1) println("Понравилось ${likes} человеку") else println("Понравилось ${likes} людям")
 
 }
